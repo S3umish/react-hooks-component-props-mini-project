f994455a2d50e12b30b95ea0f79da685fbcb5d2b
sunitee <suniteebajpai2@gmail.com>
2021-07-18 12:29:47 -0400

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,24 @@
 import React from "react";
 import blogData from "../data/blog";
+import About from './About'
+import Header from './Header'
+import ArticleList from './ArticleList'
+
 
 console.log(blogData);
 
 function App() {
+
+
   return (
     <div className="App">
       You're on your own from here! Follow the deliverables; test things out in
       the browser as you write your code; and good luck!
+     <About about={blogData.about} image={blogData.image}/>
+     <Header name={blogData.name} />
+     <ArticleList posts={blogData.posts}/>
+
+
     </div>
   );
 }
---
+++ src/components/ArticleList.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import Article from './Article'
+
+export default function ArticleList({posts}) {
+
+
+    const articleArray = posts.map((post) => {
+    return(
+
+        <Article
+        
+        key={post.id}
+        title={post.title}
+        date={post.date}
+        preview={post.preview}
+        minutes={post.minutes}
+    
+        />
+        
+        )
+    })
+
+    return (
+           <main>
+           {articleArray}
+           </main> 
+      
+    )
+}
